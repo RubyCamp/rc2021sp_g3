2a7cde851f842e10a5f12a9ebe287f1c6c662c29
Syun <surarin1515@icloud.com>
2021-03-17 10:09:29 +0900

    set score

--- main.rb
+++ main.rb
@@ -1,11 +1,13 @@
 require 'dxruby'
 
+require_relative 'score'
 Window.width = 600
 Window.height = 450
 Window.caption = "RubyCamp2021 Team3"
 
-back = Image.load("images/back.png")
-
+# back = Image.load("images/back.png")
+score = Score::Score.new
 Window.loop do
-    Window.draw(0, 0, back)
+    score.cal
+    # Window.draw(0, 0, back)
 end
