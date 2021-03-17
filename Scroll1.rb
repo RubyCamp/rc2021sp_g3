6ec3e12eb1b86e923d53485edfa4c1a22d16771d
flat-259 <79969566+flat-259@users.noreply.github.com>
2021-03-17 18:13:33 +0900

    スクロール・改
    バグがなくなったので更新します。

--- Scroll1.rb
+++ Scroll1.rb
@@ -1,13 +1,20 @@
 module Game
-  class Director < DirectorBase
+  class Scroll1 
     def initialize
       player_img =  Image.load("images/frog.png")
       @map = Map.new(50, 50, 2, 5, 15)
-      @map.set_scroll_direction(1, -1)
+      @map.set_scroll_direction(1, 1)
       @player = Player.new(10, 10, player_img, @map)
 
     end
+    def cal
+      @count_x -= self.sp_x
+      if @count_x < 0
+        @count_x += MapChip::CHIP_SIZE * self.sp_x
+        @mx += self.sp_x
+      end
 
+    end
     def reload
     end
 
