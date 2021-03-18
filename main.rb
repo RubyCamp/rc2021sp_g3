1c62773f2ed8ce58cd7ce87601b2eb6a4a8d7c1a
Kusuke023 <56672490+Kusuke023@users.noreply.github.com>
2021-03-18 13:16:13 +0900

    playerをfrogに置き換え

--- main.rb
+++ main.rb
@@ -1,10 +1,8 @@
 require 'dxruby'
 require_relative 'frog'
 require_relative 'snake'
-
-require_relative 'player'
-require_relative 'enemy'
 require_relative 'score'
+
 Width = 600
 Height = 450
 
@@ -13,44 +11,36 @@ Window.height = Height
 Window.caption = "RubyCamp2021 Team3"
 
 back = Image.load("images/back.png")
-image = Image.load("images/player.png")
-image.set_color_key([255, 255, 255])
 
-player = Player.new(250, 340, image, 10, 3)
+frog_image = Image.load("images/frog_m2.png")
+frog_image.set_color_key(C_WHITE)
 
-frog = Frog.new(100, 380)
-snake = Snake.new(500, 380)
+snake = Image.load("images/snake_m2.png")
+snake.set_color_key(C_WHITE)
 
-enemy = Image.load("images/enemy.png")
-enemy.set_color_key([255, 255, 255])
 
+frog = Frog.new(100, 380, frog_image, 3)
 
 100.times do
-    Enemy.add(rand(500..10000), 340, enemy, 5)
+    Snake.add(rand(500..10000), 380, snake)
 end
 score = Score::Score.new
 Window.loop do
     Window.draw(0, 0, back)
     score.cal
     break if Input.key_push?(K_ESCAPE)
-    # Window.draw(500, 340, enemy)
+
     frog.update
     frog.draw
 
-    player.update
-    player.draw
-
-    snake.update
-    snake.draw
-
-    Enemy.collision.each do |enemy|
-        enemy.update
-        enemy.draw
+    Snake.collision.each do |snake|
+        snake.update
+        snake.draw
     end
 
-    Sprite.check(player, Enemy.collision)
-    player.frameout(Width)
+    Sprite.check(frog, Snake.collision)
 
-    break if player.life == 0
+
+    break if frog.life == 0
 
 end 
