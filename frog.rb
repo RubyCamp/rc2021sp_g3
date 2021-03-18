3b2730d29d70b3d998805f4520b82683208a8027
ripopo <galstaff.ny@gmail.com>
2021-03-18 09:46:41 +0900

    jump_hit-wall

--- frog.rb
+++ frog.rb
@@ -8,13 +8,15 @@ class Frog < Sprite
         self.image.set_color_key(C_WHITE)
 
         @floor = 380
-        @ceiling = 300
+        @ceiling = 320
+        @wall_l = 0
+        @wall_r = 600
 
         @jumping = false
         @direction = 0
 
         t = 2
-        angle = 60
+        angle = 45
         @g = 1
         @dx = Math.cos((angle) / 180.0 * Math::PI) * t
         @dy = Math.sin((angle) / 180.0 * Math::PI) * t
@@ -22,6 +24,7 @@ class Frog < Sprite
 
     def update
         @jumping = true if Input.key_push?(K_SPACE)
+        hit_wall
         if @jumping then
             jumping
             # if self.y <= @floor then
@@ -40,15 +43,21 @@ class Frog < Sprite
             self.x += @dx
             self.y -= @dy
         end
-        
         @jumping = false
     end
 
     def walking
         @direction = Input.x * 3
-
         if self.y <= @floor then
             self.y += @dy
+        end
+    end
+
+    def hit_wall
+        if self.x <= @wall_l then
+            self.x = @wall_l
+        elsif (self.x + 32) >= @wall_r then
+            self.x = @wall_r - 32
         end
     end
 end
